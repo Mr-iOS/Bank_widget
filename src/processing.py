c848f362b2016c6ef1ac66fe08942c1490d4f6e2
Sergey Kozlov <sergei.k.2012@mail.ru>
2025-03-28 15:22:05 +0300

    add new func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -4,6 +4,11 @@ def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[
     return [i for i in list_of_dicts if i["state"] == state]
 
 
+def sort_by_date(list_of_dicts: list[dict], is_reverse: bool = True) -> list[dict]:
+    """Функция возвращает новый список, отсортированный по дате"""
+    return sorted(list_of_dicts, key=lambda x: x["date"], reverse=is_reverse)
+
+
 if __name__ == "__main__":
     test_list = [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -13,3 +18,6 @@ if __name__ == "__main__":
     ]
     print(filter_by_state(test_list))
     print(filter_by_state(test_list, "CANCELED"))
+    print("-" * 150)
+    print(sort_by_date(test_list))
+    print(sort_by_date(test_list, False))
